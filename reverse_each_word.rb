7efcbe39c32312276c1f2b7d32ad6f0e015df0f5
Russell Sutter <russell.sutter@gmail.com>
2020-07-14 23:59:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,8 @@ def reverse_each_word(string)
   array = []
   
   string.each do |word|
-    array << word.reverse
+    reverse_word = word.reverse
+    array << reverse_word
   end
   array.join("")
 end
