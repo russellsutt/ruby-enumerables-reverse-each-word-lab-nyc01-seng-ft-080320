9cac843310c3b68fcd50ac7bace746981e5491e4
Russell Sutter <russell.sutter@gmail.com>
2020-07-15 00:08:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,9 @@
 require 'pry'
 
 def reverse_each_word(string)
+  array
   
-  array1 = []
-  array2 = []
-  
-  array1 << string.split
+  array << string.split
   
   array1.each do |word|
     array2 << word.reverse
